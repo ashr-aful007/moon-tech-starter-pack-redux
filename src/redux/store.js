276d9f3432883a14f3d03a5734a4fec749576fdf
ashr-aful007 <ashraful99980@gmail.com>
2023-08-12 16:00:43 +0600

    add curry function and install thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,13 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import productReducer from "./reducers/productReducer";
 import rootReducr from "./reducers/rootReducer";
+import logger from "redux-logger";
+import cartCounter from "./middlewares/cartCounter";
+import thunk from "redux-thunk";
 
-const store = createStore(rootReducr, composeWithDevTools());
+
+// composeWithDevTools()
+const store = createStore(rootReducr, composeWithDevTools(applyMiddleware(cartCounter, thunk, logger)));
 
 export default store;
